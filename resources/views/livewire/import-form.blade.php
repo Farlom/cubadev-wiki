087db4b4770b27aa61e1d3590bc6b996671b78a1
Stepan Farlom <stepan.farlom@gmail.com>
2024-10-17 18:10:30 +0300

    animate buttons

--- resources/views/livewire/import-form.blade.php
+++ resources/views/livewire/import-form.blade.php
@@ -2,7 +2,7 @@
     <div class="flex flex-col">
         <div class="flex flex-row gap-5">
             <input
-                class="p-2 rounded-2xl border-2 border-white w-48  focus:outline-0 focus:placeholder:text-sm @error('title') border-red-500/50 @enderror"
+                class="p-2 rounded-2xl border-2 border-white w-48  focus:outline-0 focus:placeholder:text-sm @error('title') border-red-500/50 @enderror placeholder:duration-300"
                 type="text" wire:model="title" placeholder="Название статьи">
             <button
                 class="transition ease-in-out duration-300 w-32 bg-blue-400 text-white rounded-2xl p-2 hover:bg-blue-300 active:bg-blue-200"
